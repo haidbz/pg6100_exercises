deeb252f6c0576340c561db9d77d75a47a519178
Håvard Tveiterås Meistad <haidbz@gmail.com>
2016-11-19 13:37:01 +0100

    Entities done

--- quiz/src/main/java/entities/Category.java
+++ quiz/src/main/java/entities/Category.java
@@ -1,12 +1,59 @@
 package entities;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Håvard on 04.11.2016.
  */
 @Entity
 public class Category {
-    String name;
-    Category parentCategory;
+    @Id
+    @NotBlank
+    private String name;
+    
+    @ManyToOne
+    private Category parentCategory;
+    
+    @OneToMany
+    private Category childCategories;
+    
+    @OneToMany
+    private List<Quiz> quizzes;
+    
+    public List<Quiz> getQuizzes() {
+        if (quizzes == null)
+            quizzes = new ArrayList<>();
+        return quizzes;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Category getParentCategory() {
+        return parentCategory;
+    }
+    
+    public void setParentCategory(Category parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+    
+    public Category getChildCategories() {
+        return childCategories;
+    }
+    
+    public void setChildCategories(Category childCategories) {
+        this.childCategories = childCategories;
+    }
 }
--- quiz/src/main/java/entities/Quiz.java
+++ quiz/src/main/java/entities/Quiz.java
@@ -1,13 +1,65 @@
 package entities;
 
-import javax.persistence.Entity;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by Håvard on 04.11.2016.
  */
 @Entity
 public class Quiz {
-    String[] answers;
-    int correctAnswer;
-    Category subSubCategory;
+    @Id @GeneratedValue
+    private Long quizId;
+    
+    @Column(unique = true)
+    @NotBlank
+    private String question;
+    
+    @NotBlank
+    private String[] answers;
+    
+    @Min(value = 0)
+    @Max(value = 3)
+    private int correctAnswer;
+    
+    @ManyToOne
+    @ParentCategories(parents = 2)
+    @NotNull
+    private Category subSubCategory;
+    
+    public String getQuestion() {
+        return question;
+    }
+    
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    
+    public String[] getAnswers() {
+        return answers;
+    }
+    
+    public void setAnswers(String[] answers) {
+        this.answers = answers;
+    }
+    
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+    
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+    
+    public Category getSubSubCategory() {
+        return subSubCategory;
+    }
+    
+    public void setSubSubCategory(Category subSubCategory) {
+        this.subSubCategory = subSubCategory;
+    }
 }
