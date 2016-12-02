ea2c99d3b05ceb5b414042ed3a541bdadc90c997
haidbz <haidbz@gmail.com>
2016-12-02 21:45:17 +0100

    Proplem solved! Yay! Now onto reading Savage Divinity.

--- rest_api/src/main/java/meistad/pg6100/rest_api/dto/QuizDTO.java
+++ rest_api/src/main/java/meistad/pg6100/rest_api/dto/QuizDTO.java
@@ -1,35 +1,1 @@
-package meistad.pg6100.rest_api.dto;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-/**
- * Created by Håvard on 25.11.2016.
- */
-@ApiModel("A quiz")
-public class QuizDTO {
-    @ApiModelProperty("The id of the quiz")
-    public Long id;
-    
-    @ApiModelProperty("The question of the quiz")
-    public String question;
-    
-    @ApiModelProperty("The answer options of the quiz")
-    public String[] answers;
-    
-    @ApiModelProperty("The index of the correct answer among the answers")
-    public int correctAnswer;
-    
-    @ApiModelProperty("The category of the quiz")
-    public String category;
-    
-    public QuizDTO() {}
-    
-    public QuizDTO(Long id, String question, String[] answers, int correctAnswer, String category) {
-        this.id = id;
-        this.question = question;
-        this.answers = answers;
-        this.correctAnswer = correctAnswer;
-        this.category = category;
-    }
-}
+package meistad.pg6100.rest_api.dto;import io.swagger.annotations.ApiModel;import io.swagger.annotations.ApiModelProperty;/** * Created by Håvard on 25.11.2016. */@ApiModel("A quiz")public class QuizDTO {    @ApiModelProperty("The id of the quiz")    public Long id;        @ApiModelProperty("The question of the quiz")    public String question;        @ApiModelProperty("The answer options of the quiz")    public String[] answers;        @ApiModelProperty("The index of the correct answer among the answers")    public int correctAnswer;        @ApiModelProperty("The category of the quiz")    public String category;        public QuizDTO() {}        public QuizDTO(Long id, String question, String[] answers, int correctAnswer, String category) {        this.id = id;        this.question = question;        this.answers = answers;        this.correctAnswer = correctAnswer;        this.category = category;    }}
